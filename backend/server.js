1e8f5763357d9d8ba93affcbbb1c5c17b77283d8
ASHiSHxNagar <ashishnagar043@gmail.com>
2025-01-26 02:06:28 +0530

    Database Connection Created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("server is runnig")
+import express from 'express'
+import authRoutes from "./routes/auth.routes.js"
+const app = express()
+import dotenv from "dotenv"
+
+dotenv.config()
+
+app.use("/api/auth", authRoutes)
+
+app.listen(8000, () => {
+    console.log("server is runnign on port 8000")
+})
