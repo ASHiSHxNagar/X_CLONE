5c2d87f44699c702a9c4541efdf989dd80b9b5d5
ASHiSHxNagar <ashishnagar043@gmail.com>
2025-01-27 01:22:36 +0530

    1-auth controllers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,119 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generatetoken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
+
 export const signup = async (req, res) => {
-    res.json({
-        data: "you hit the signup endpoint"
-    })
+
+    try {
+
+
+        const { fullName, username, password, email } = req.body;
+
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "invalid emial format" })
+        }
+
+        const exisitingUser = await User.findOne({ username })
+        if (exisitingUser) {
+            return res.status(400).json({ error: "username is already taken" })
+        }
+
+        const exisitingEmail = await User.findOne({ email })
+        if (exisitingUser) {
+            return res.status(400).json({ error: "email is already taken" })
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            username,
+            email,
+            password: hashedPassword
+        })
+        if (password.length < 6) {
+            return res.status(400).json({ error: "password must be at least 6 characters long" })
+        }
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res)
+            await newUser.save()
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                followers: newUser.followers,
+                following: newUser.email,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg,
+            })
+        } else {
+            res.status(400).json({ error: "invalid user data" })
+        }
+    }
+
+    catch (error) {
+        console.log("error in signup controller", error.message)
+        res.status(500).json({ error: "internal server error" })
+
+    }
 }
-export const login = (req, res) => {
-    res.json({
-        data: "you hit the login endpoint"
-    })
+
+
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username })
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({ error: "invalid username or password" })
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            followers: user.followers,
+            following: user.email,
+            profileImg: user.profileImg,
+            coverImg: user.coverImg,
+        })
+    }
+
+    catch (error) {
+        console.log("error in login controller", error.message)
+        res.status(500).json({ error: "internal server error" })
+
+    }
 }
-export const logout = (req, res) => {
-    res.json({
-        data: "you hit the logout endpoint"
-    })
+
+
+export const logout = async (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "logout succesfully" })
+    } catch (error) {
+        console.log("error in logout controller", error.message);
+        res.status(500).json({ error: "internal server error" })
+    }
+}
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select("-password")
+        res.status(200).json(user);
+
+    }
+    catch (error) {
+        console.log("error in getMe controller", error.message);
+        res.status(500).json({ error: "internal server error" })
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ import authRoutes from "./routes/auth.routes.js"
 const app = express()
 import dotenv from "dotenv"
 import connectMongoDB from './db/connectMongoDB.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
+app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 const PORT = process.env.PORT || 5000
