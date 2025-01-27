ebf4b5e721dd4248aad09470832db4fa62221956
ASHiSHxNagar <ashishnagar043@gmail.com>
2025-01-28 01:28:47 +0530

    1-post routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from 'cloudinary'
 
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
+import postRoutes from "./routes/post.routes.js"
 
 
 const app = express()
@@ -22,6 +23,7 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("server is runnign on port 5000")
