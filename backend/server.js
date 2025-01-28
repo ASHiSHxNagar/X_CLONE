434f29c2044404a501f71766028c1b8d7e043a8a
ASHiSHxNagar <ashishnagar043@gmail.com>
2025-01-29 02:00:22 +0530

    3-payload limit updated

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
